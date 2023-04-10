ee65ddee8324d1c30ee8d3a7ba15111684c071db
szaranczuk <igor.hanczaruk@gmail.com>
2023-04-10 23:39:49 +0200

    Written implementation for lcp array

--- src/lcp_array/lcp_array.c
+++ src/lcp_array/lcp_array.c
@@ -1,8 +1,26 @@
+/**********************************************************************************
+ *     Description: Algorithm for computing suffix LCP array of Suffix Array      *
+ *                            Time complexity: O(nlogn)                           *
+ *                           Memory complexity: O(nlogn)                          *
+ *                             Author: Igor Hanczaruk                             *
+ *                               Date: 10.04.2023                                 *
+ **********************************************************************************/
+
 #include <stdlib.h>
 #include <list.h>
 #include <lcp_array.h>
 
-int* lcp_array(int** c, int* p,int n);
+int* lcp_array(int** c, int* p, int n)
+{
+	int lg = __builtin_clz(1) - __builtin_clz(n);
+	if (__builtin_popcount(n) != 1) lg++;
+	int* ret = malloc((n - 1) * sizeof(int));
+	for (int i = 1; i < n; i++)
+	{
+		ret[i-1] = lcp(c, p[i-1], p[i], lg);
+	}
+	return ret;
+}
 
 int lcp(int** c, int a, int b, int lg)
 {
