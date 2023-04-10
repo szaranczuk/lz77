d4d424e280a1f1619c75cb65b0ee8703344b6beb
szaranczuk <igor.hanczaruk@gmail.com>
2023-04-10 20:40:11 +0200

    Added random suffix array tests

--- tests/suffix_array_test_auto.cpp
+++ tests/suffix_array_test_auto.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <string>
 #include <deque>
+#include <cassert>
 
 extern "C" int* suffix_array(const char*, int***);
 
@@ -21,7 +22,7 @@ std::vector<int> suffix_array_brute(const std::string& input_string)
     std::vector<std::pair<std::deque<char>, int>> suff;
     std::deque<char> current_suffix;
     int n = input_string.size();
-    for (int i = n-1; i >= 0; i++)
+    for (int i = n-1; i >= 0; i--)
     {
         current_suffix.push_front(input_string[i]);
         suff.push_back({current_suffix, i});
@@ -43,8 +44,13 @@ int main(int argc, char** argv)
     std::string random_string;
     for (int i = 0; i < n; i++)
     {
-        random_string.push_back((char) getrand(1, 255));
+        random_string.push_back(getrand(1, 127));
     }
     int** c;
-    int* p = suffix_array(random_string.c_str(), &c);
+    int* result = suffix_array(random_string.c_str(), &c);
+    std::vector<int> answer = suffix_array_brute(random_string);
+    for (int i = 0; i < n; i++)
+    {
+        assert(result[i] == answer[i]);
+    }
 }
